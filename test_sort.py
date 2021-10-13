4fb0e3581f3f29cb62b63809f71fe04d3bfb0b59
Jose <jose.garcia@pregrado.uoh.cl.com>
2021-10-13 17:24:39 -0300

    se agrega tests para insertion_sort

--- test_sort.py
+++ test_sort.py
@@ -27,6 +27,44 @@ class TestSortingAlgorithms(unittest.TestCase):
         sort.quick_sort(a)
         self.assertEqual(a, [1])
 
+    def test_quick_sort_4(self):
+        """
+        Prueba para lista de tamaño 1
+        """
+        a = [1,6,2,7,2,4,1]
+        sort.insertion_sort(a)
+        self.assertEqual(a, [1,1,2,2,4,6,7])
 
+    def test_insertion_sort_1(self):
+        """
+        Prueba para lista de tamaño 1
+        """
+        a = [1,6,2,7,2,4,1]
+        sort.insertion_sort(a)
+        self.assertEqual(a, [1,1,2,2,4,6,7])
+
+    def test_insertion_sort_2(self):
+        """
+        Prueba para lista de tamaño 1
+        """
+        a = [2, 14, 4, 8, 9]
+        sort.insertion_sort(a)
+        self.assertEqual(a, [2,4,8,9,14])
+        
+    def test_insertion_sort_3(self):
+        """
+        Prueba para lista de tamaño 1
+        """
+        a = [0,42,42,42,1,7,3,4]
+        sort.insertion_sort(a)
+        self.assertEqual(a, [0,1,3,4,7,42,42,42])
+        
+    def test_insertion_sort_4(self):
+        """
+        Prueba para lista de tamaño 1
+        """
+        a = [10, 14, 1, 11, 13]
+        sort.insertion_sort(a)
+        self.assertEqual(a, [1,10,11,13,14])
 if __name__ == '__main__':
     unittest.main()
