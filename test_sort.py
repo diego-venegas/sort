e4a996ad11a6cbca45c4d15f88ef9c3cd06ae63b
Diego Valdés <diego_alejo7@hotmail.com>
2021-10-13 18:04:33 -0300

    close #7; Implementar tests para  mergesort

--- test_sort.py
+++ test_sort.py
@@ -37,5 +37,38 @@ class TestSortingAlgorithms(unittest.TestCase):
         self.assertEqual(a, [0, 0, 0, 0, 1, 1, 1, 1])
 
 
+    def test_merge_sort_1(self):
+        """
+        Prueba caso general #1
+        """
+        a = [1, 4, 6, 1, 2, 0]
+        sort.merge_sort(a)
+        self.assertEqual(a, [0, 1, 1, 2, 4, 6])
+
+    def test_merge_sort_2(self):
+        """
+        Prueba caso general #2
+        """
+        a = [2, 4, 9, 2, 2, 7]
+        sort.merge_sort(a)
+        self.assertEqual(a, [2, 2, 2, 4, 7, 9])
+
+    def test_merge_sort_3(self):
+        """
+        Prueba para lista de tamaño 1
+        """
+        a = [1]
+        sort.merge_sort(a)
+        self.assertEqual(a, [1])
+
+    def test_merge_sort_4(self):
+        """
+        Prueba caso general #3
+        """
+        a = [0, 0, 1, 1, 1, 1, 0, 0]
+        sort.merge_sort(a)
+        self.assertEqual(a, [0, 0, 0, 0, 1, 1, 1, 1])
+
+
 if __name__ == '__main__':
     unittest.main()
